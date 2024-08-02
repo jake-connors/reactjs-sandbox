82dfaec0d76da1292fede30377643b5dcf62892c
jake-connors <jakedavidconnors@gmail.com>
2024-08-02 08:34:22 -0400

    working on react v17 i guess

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { rootReducer, initial } from "./redux/reducers";
@@ -14,7 +14,7 @@ const store = createStore(
         thunkMiddleware
     )
 );
-render(
+ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
